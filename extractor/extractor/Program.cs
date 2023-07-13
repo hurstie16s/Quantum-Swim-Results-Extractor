248655c5cc5a00fddabf9df238374ad81e274463
hurstie16s <samuel.hurst2@btinternet.com>
2023-07-13 10:23:39 +0100

    Project Set up and start

---
+++ extractor/extractor/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using UglyToad.PdfPig;
+
+namespace extractor
+{
+internal static class ExtractorMain
+{
+  public static void Main(string[] args)
+  {
+    // Check right number of arguments
+    if (args.Length != 2)
+    {
+      Console.Error.WriteLine("Incorrect Number of Arguments: Press Enter to Exit");
+      Console.Read();
+      Environment.Exit(1);
+    }
+    var sourceDirectory = args[0];
+    var destinationDirectory = args[1];
+    
+    ExtractFiles(sourceDirectory);
+  }
+
+  private static void ExtractFiles(string fileDirectory)
+  {
+    var files = Directory.GetFiles($"{fileDirectory}\\");
+  }
+}
+}
